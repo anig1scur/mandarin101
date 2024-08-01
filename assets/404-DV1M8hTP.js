d665b01b57c70ee6f6bd3b63062c78346454bb5a
anig1scur <anig1scur@users.noreply.github.com>
2024-08-01 10:11:38 +0000

    deploy: 45dd9a4a08782dee11fe12616da899639608fc82

---
+++ assets/404-DV1M8hTP.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Bb0k9PXM.js";import"./react-vendor-DxtPTlJ_.js";function i(){return t.jsx("div",{children:"404"})}export{i as default};
--- assets/404-uFeKnOv_.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-BuAUXrx1.js";import"./react-vendor-DxtPTlJ_.js";function i(){return t.jsx("div",{children:"404"})}export{i as default};
